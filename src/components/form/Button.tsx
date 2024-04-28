a81731e14512ec908a59a8ed7bc31c5d608d8318
MMMJB <michaelbeck445@gmail.com>
2024-04-28 15:12:09 -0400

    button component

---
+++ src/components/form/Button.tsx
@@ -0,0 +1,41 @@
+const styleMap = {
+  sm: "px-5 py-2",
+  md: "px-7 py-3 text-xl",
+  lg: "px-8 py-3 text-2xl",
+};
+
+const variantMap = {
+  primary:
+    "bg-button hover:bg-button-hover disabled:bg-button-disabled focus:bg-button-pressed text-white",
+  outline:
+    "bg-button-surface border border-button text-brand-accent hover:border-button-hover hover:text-brand-tertiary focus:border-button-pressed focus:text-button-pressed disabled:text-brand-quaternary disabled:border-button-disabled disabled:text-button-disabled",
+};
+
+export default function Button({
+  children,
+  className,
+  disabled,
+  onClick,
+  type = "button",
+  size = "md",
+  variant = "primary",
+}: {
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  variant?: "primary" | "outline";
+  size?: "sm" | "md" | "lg";
+}) {
+  return (
+    <button
+      className={`rounded-lg w-full bg-button hover:bg-button-hover disabled:bg-button-disabled focus:bg-button-pressed text-white font-medium transition-colors ${styleMap[size]} ${variantMap[variant]} ${className}`}
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+}

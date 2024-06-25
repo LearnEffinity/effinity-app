d1e1786ed31a9e761c2ef8414db0d897243efa69
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-06-25 12:24:27 -0500

    huh

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,1 +1,17 @@
-// just a file
+import React from "react";
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <>
+      <nav className="flex flex-col px-8 ">
+        <Image
+          alt="Effinity Logo"
+          src="/effinity-logo.png"
+          width={156}
+          height={40}
+        />
+      </nav>
+    </>
+  );
+}

d9c61e94f020d719b4ccc14b1fd1ca91817d30bf
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-06-28 16:38:41 -0500

    Added link to sideItem, added more props to interface

--- src/components/dashboard/sidebarItem.tsx
+++ src/components/dashboard/sidebarItem.tsx
@@ -1,54 +1,49 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 import icons from "./icons";
 
 interface SidebarItemProps {
   icon: keyof typeof icons;
   name: string;
   isCollapsed: boolean;
-  onClick?: () => void;
+  isSelected: boolean;
+  route: string;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
   icon,
   name,
   isCollapsed,
-  onClick,
+  isSelected,
+  route,
 }) => {
-  const [isSelected, setIsSelected] = useState(false);
   const Icon = icons[icon];
 
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
-    } else {
-      setIsSelected(!isSelected);
-    }
-  };
-
   return (
-    <motion.div
-      className={`mx-auto my-4 flex cursor-pointer rounded-xl py-3 transition-colors duration-200 ease-in-out
+    <Link href={route} passHref>
+      <motion.div
+        className={`mx-auto my-4 flex cursor-pointer rounded-xl py-3 transition-colors duration-200 ease-in-out
         ${isSelected ? "bg-brand-accent text-white" : "hover:bg-gray-100"}
         ${isCollapsed ? " justify-start" : "justify-start px-8"}`}
-      onClick={handleClick}
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-    >
-      <div className={`flex items-start ${isCollapsed ? "mx-auto" : ""}`}>
-        <Icon
-          strokeColor={isSelected ? "#fff" : "#080808"}
-          className={`h-6 w-6 ${isSelected ? "text-white" : "text-gray-600"}`}
-        />
-        {!isCollapsed && (
-          <span
-            className={`font-medium ${isSelected ? "text-white" : "text-gray-800"} ${isCollapsed ? "mx-auto" : "ml-4"} `}
-          >
-            {name}
-          </span>
-        )}
-      </div>
-    </motion.div>
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        <div className={`flex items-start ${isCollapsed ? "mx-auto" : ""}`}>
+          <Icon
+            strokeColor={isSelected ? "#fff" : "#080808"}
+            className={`h-6 w-6 ${isSelected ? "text-white" : "text-gray-600"}`}
+          />
+          {!isCollapsed && (
+            <span
+              className={`font-medium ${isSelected ? "text-white" : "text-gray-800"} ${isCollapsed ? "mx-auto" : "ml-4"} `}
+            >
+              {name}
+            </span>
+          )}
+        </div>
+      </motion.div>
+    </Link>
   );
 };
 

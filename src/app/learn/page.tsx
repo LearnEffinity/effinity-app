6a7d4c6552dfbb535da724b1cf76eb6d6751dea5
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-07-05 13:03:35 -0500

    added continue section on learn page

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Sidebar from "@/components/dashboard/sidebar";
+import Continue from "@/components/dashboard/learn/continue";
 import { usePathname } from "next/navigation";
 
 export default function Home() {
@@ -10,23 +11,16 @@ export default function Home() {
     <div className="flex min-h-screen">
       <Sidebar currentRoute={pathname} />
       <main className="flex-1 px-8 py-10">
+        {/* MAIN CONTENT */}
         <div className="mx-auto ">
           <div className="mb-8 flex items-center justify-between">
             <h1 className="text-5xl font-bold text-brand-quaternary">
               LEARN PAGE
             </h1>
           </div>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
-            assumenda quos sit quas omnis impedit praesentium corrupti, autem
-            nostrum sequi eaque molestias mollitia quam. Quae tempora quas
-            quisquam deserunt nobis! Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Quod cupiditate ipsa reprehenderit eligendi
-            corporis dolorum! Omnis quidem eos sed in necessitatibus ut?
-            Inventore molestiae aspernatur quia repudiandae alias, illum
-            repellendus.
-          </p>
+          <Continue />
         </div>
+        {/* MAIN CONTENT */}
       </main>
     </div>
   );

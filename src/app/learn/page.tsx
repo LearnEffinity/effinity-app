9de5d5a05c66fd6524e3b9ddd8db7b8b425e26ce
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-07-05 19:38:52 -0500

    added new prop, top navbar, and recommended section

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,26 +1,39 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "@/components/dashboard/sidebar";
 import Continue from "@/components/dashboard/learn/continue";
+import Recommended from "@/components/dashboard/learn/recommended";
 import { usePathname } from "next/navigation";
+import TopNav from "@/components/dashboard/topnav";
 
 export default function Home() {
   const pathname = usePathname();
+  const [sidebarWidth, setSidebarWidth] = useState(245);
+
+  const handleSidebarWidthChange = (width: number) => {
+    setSidebarWidth(width);
+  };
 
   return (
     <div className="flex min-h-screen">
-      <Sidebar currentRoute={pathname} />
-      <main className="flex-1 px-8 py-10">
-        {/* MAIN CONTENT */}
-        <div className="mx-auto ">
-          <div className="mb-8 flex items-center justify-between">
-            <h1 className="text-5xl font-bold text-brand-quaternary">
-              LEARN PAGE
-            </h1>
+      <div className="fixed">
+        <Sidebar
+          currentRoute={pathname}
+          onWidthChange={handleSidebarWidthChange}
+        />
+      </div>
+      <main className="flex-1" style={{ marginLeft: `${sidebarWidth}px` }}>
+        <div className="px-8 py-10">
+          {/* MAIN CONTENT */}
+          <div className="mx-auto ">
+            <div className="mb-8 flex items-center justify-between">
+              <TopNav />
+            </div>
+            <Continue />
+            <Recommended />
           </div>
-          <Continue />
+          {/* MAIN CONTENT */}
         </div>
-        {/* MAIN CONTENT */}
       </main>
     </div>
   );

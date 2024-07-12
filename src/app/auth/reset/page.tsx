650f9d89281024373e7d1808984df4eb1d092dc5
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-07-11 23:47:23 -0500

    Added a suspense boundary for the reset page

--- src/app/auth/reset/page.tsx
+++ src/app/auth/reset/page.tsx
@@ -1,80 +1,22 @@
-"use client";
-import React, { useState, useEffect, use } from "react";
-import { createClient } from "@/utils/supabase/client";
-import { useSearchParams, useRouter } from "next/navigation";
+import React, { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import { InputWithLabel } from "@/components/form/Input";
-import Button from "@/components/form/Button";
+const ResetPasswordForm = dynamic(
+  () => import("@/components/form/ResetPassword"),
+  {
+    ssr: false,
+  },
+);
 
-function ResetPasswordPage() {
-  const supabase = createClient();
-  const [validSession, setValidSession] = useState(false);
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
-  const passwordsMatch = newPassword === confirmPassword;
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const code = searchParams.get("code");
-  if (!code) {
-    return <p>Invalid reset code.</p>;
-  }
-  useEffect(() => {
-    async function checkSession() {
-      const { data, error } = await supabase.auth.exchangeCodeForSession(code);
-      if (error) {
-        console.error("Error validating reset code:", error.message);
-        return;
-      }
-      setValidSession(true);
-      const session = await supabase.auth.getSession()
-      console.log("Session:", session);
-
-    }
-    checkSession();
-  }
-  , [code]);
-  const handleResetPassword = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (newPassword !== confirmPassword) {
-      setError("Passwords do not match.");
-      return;
-    }
-    const { data, error } = await supabase.auth.updateUser({
-      password: newPassword,
-    });
-    if (error) {
-      setError(error.message);
-      return;
-    }
-    setError("");
-    router.push("/");
-  };
-
+export default function ResetPasswordPage() {
   return (
     <div className="flex h-full flex-col gap-8 py-8">
       <hgroup className="flex flex-col gap-1">
         <h1 className="text-4xl font-semibold">Reset Your Password</h1>
       </hgroup>
-      <form onSubmit={handleResetPassword} className="flex flex-col gap-4">
-        <InputWithLabel
-          label="New Password"
-          value={newPassword}
-          onChange={(v) => setNewPassword(v)}
-          type="password"
-        />
-        <InputWithLabel
-          label="Confirm Password"
-          value={confirmPassword}
-          onChange={(v) => setConfirmPassword(v)}
-          type="password"
-        />
-        <Button className="mt-4" type="submit">
-          Reset Password
-        </Button>
-      </form>
+      <Suspense fallback={<p>Loading form...</p>}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   );
 }
-
-export default ResetPasswordPage;

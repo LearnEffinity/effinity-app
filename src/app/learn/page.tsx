445a02d87e39532a40db71705d27b63ddac2a273
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-07-13 15:55:04 -0500

    fix: Migrate layout stuff to page to overide layout

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,22 +1,40 @@
 "use client";
-
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "@/components/dashboard/sidebar";
+import { usePathname } from "next/navigation";
 import Continue from "@/components/dashboard/learn/continue";
 import Recommended from "@/components/dashboard/learn/recommended";
 import TopNav from "@/components/dashboard/topnav";
 
 export default function Home() {
+  const pathname = usePathname();
+  const [sidebarWidth, setSidebarWidth] = useState(245);
+
+  const handleSidebarWidthChange = (width: number) => {
+    setSidebarWidth(width);
+  };
+
   return (
-    <>
-      {/* MAIN CONTENT */}
-      <div className="mx-auto ">
-        <div className="mb-8 flex items-center justify-between">
-          <TopNav />
-        </div>
-        <Continue />
-        <Recommended />
+    <div className="flex min-h-screen">
+      <div className="fixed">
+        <Sidebar
+          currentRoute={pathname}
+          onWidthChange={handleSidebarWidthChange}
+        />
       </div>
-      {/* MAIN CONTENT */}
-    </>
+      <main className="flex-1" style={{ marginLeft: `${sidebarWidth}px` }}>
+        <div className="px-8 py-10">
+          {/* MAIN CONTENT */}
+          <div className="mx-auto ">
+            <div className="mb-8 flex items-center justify-between">
+              <TopNav />
+            </div>
+            <Continue />
+            <Recommended />
+          </div>
+          {/* MAIN CONTENT */}
+        </div>
+      </main>
+    </div>
   );
 }

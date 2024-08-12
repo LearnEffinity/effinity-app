63a4f5e75a5c2376e9b6855307411b0f5fa67224
LandonHarter <landonharter@outlook.com>
2024-08-12 10:08:29 -0600

    who knew a sidebar logo could be so frustrating

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -47,18 +47,15 @@ const Sidebar: React.FC<SidebarProps> = ({ currentRoute, onWidthChange }) => {
       animate={{ width: isCollapsed ? 100 : 245 }}
       transition={{ duration: 0.25, ease: "easeInOut" }}
     >
-      <div
-        className={`mt-5 flex-col ${isCollapsed ? "" : ""} items-center justify-center`}
-      >
+      <div className={`mt-5 flex flex-col items-center justify-center`}>
         <Image
           src={isCollapsed ? "/logo.svg" : "/effinity-logo.png"}
+          className={`!h-[48px] transition-transform ${isCollapsed ? "!w-[48px] !max-w-[48px]" : "w-fit"}`}
           alt="Effinity Logo"
-          width={isCollapsed ? 40 : 156}
-          height={40}
+          width={168}
+          height={52}
         />
-        <div
-          className={`${isCollapsed ? "ml-1" : "ml-7"} mt-6 flex justify-start `}
-        >
+        <div className={`mt-6 flex justify-start `}>
           <button className="flex items-center " onClick={toggleCollapse}>
             {isCollapsed ? (
               <icons.Collapsed className="h-7 w-7 transform transition-all duration-300 ease-in-out hover:scale-110" />
